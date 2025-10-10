d5aee72173b164ad4d421a4d647d10f859dd8eed
Simran Sharma <simran.sharma@mymobiforce.com>
2025-10-10 12:40:01 +0530

    Auth screen and whatsapp bot config screen changes by simran

--- whatsapp-ai-saas/flutter_onboarding/lib/main.dart
+++ whatsapp-ai-saas/flutter_onboarding/lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'api/api.dart';
 import 'controller/onboarding_controller.dart';
+import 'package:firebase_core/firebase_core.dart';
 // Import your new check screen and home screen
 import 'screens/check_auth_screen.dart'; // Create this
 // import 'screens/home_screen.dart'; // Create this later
@@ -14,8 +15,18 @@ String resolveApiBase() {
   // return 'https://0b0d30716cca.ngrok-free.app'; // Ensure no trailing space
 }
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+      options: const FirebaseOptions(
+          apiKey: "AIzaSyBXal3rxC8vav5BvxJqoHUqHLN_yoeV9Bw",
+          authDomain: "whatsapp-saas-3ed90.firebaseapp.com",
+          projectId: "whatsapp-saas-3ed90",
+          storageBucket: "whatsapp-saas-3ed90.firebasestorage.app",
+          messagingSenderId: "734195415255",
+          appId: "1:734195415255:web:ca824a4e8a3a97d091d892",
+          measurementId: "G-WNVHGN378H"));
   runApp(const MyApp());
 }
 
